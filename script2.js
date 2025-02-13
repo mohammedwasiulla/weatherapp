1d74ac025bcde76a4855535a715ca006b54776a4
MOHAMMED WASIULLAH <112477455+mohammedwasiulla@users.noreply.github.com>
2025-02-13 11:02:46 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,14 +1,1 @@
-const toggleButton = document.getElementById('toggleButton');
-    toggleButton.addEventListener('click', function () {
-  // Body element ko target karte hain
-    const body = document.body;
-  
-  // Agar body ka background color white hai ya set nahin hai, toh black karenge
-  if (body.style.backgroundColor === 'black') {
-    body.style.backgroundColor = 'white';
-  } else {
-    // Agar black nahi hai toh white karenge
-    body.style.backgroundColor = 'black';
-  }
-});
-'e177466e572d4138bb208c558e2a654b';
+
